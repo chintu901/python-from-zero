48e64428d28250dcc106ef953fc00f7c2b5e9f74
chintu901 <chetanranga92@gmail.com>
2025-12-27 22:18:50 +0530

    Update the course structure

--- 0. Programming-Basics/test.py
+++ 0. Programming-Basics/test.py
@@ -1,25 +1,4 @@
-def sum(a, b):
-    return a + b
+x = 3 + 2j
 
-def product(a, b):
-    return a * b
-
-def divid(a, b):
-    return a / b
-
-def sub(a, b):
-    return a - b
-
-x = int(input("Enter x: "))
-y = int(input("Enter y: "))
-
-add = sum(x, y)
-mul = product(x, y)
-div = divid(x, y)
-s = sub(x, y)
-
-print(add)
-print(mul)
-print(div)
-print(s)
-
+print(x.real)
+print(x.imag)

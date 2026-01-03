554fc88574f4f1922ad786d3d272d8069b3595c6
chintu901 <chetanranga92@gmail.com>
2026-01-03 17:41:46 +0530

    update the files

--- 0. Programming-Basics/test.py
+++ 0. Programming-Basics/test.py
@@ -1,3 +1,4 @@
-text = "I Love Java"
-print(text.replace("Java", "Python"))
-print(text.find("Love"))
+numbers = [10, 20, 30]
+print(numbers)  # [10, 20, 30]
+numbers[2] = 40
+print(numbers)  # [10, 20, 40]
